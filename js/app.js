f9113e83503d005e771ce1a63e4392c92cd1d267
Anna65236188727 <94472429+Anna65236188727@users.noreply.github.com>
2023-06-28 11:07:11 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,1 @@
-const MenuBtn = document.getElementById('toggleMenu');
-const ModalWindow = document.querySelector('.ModalWindow');
 
-
-MenuBtn.addEventListener('click', function() {
-    MenuBtn.classList.toggle('open-menu');
-    ModalWindow.classList.toggle('active');
-})
